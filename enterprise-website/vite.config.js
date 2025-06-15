1984b9cc8a56e1048528ee598c129f1201e6aa3a
KOKETSO LEGOABE <koketso.lgoabe@apex-rts.co.za>
2025-06-15 13:53:53 +0200

    Update Vite configuration base path for local development

--- enterprise-website/vite.config.js
+++ enterprise-website/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mcgeba-Enterprise/', // <-- Only add this if deploying to GitHub Pages
+  base: './', // <-- Add this
 })
